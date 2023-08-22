4d1b401c2bb072aaa032e4c18d71dc868a3db476
Amelia <ereshkigalgames@gmail.com>
2023-08-22 23:05:51 +0200

    Source upload

---
+++ src/QuestZoneAPI/Patches/GameWorldPatch.cs
@@ -0,0 +1,114 @@
+﻿using Aki.Reflection.Patching;
+using EFT;
+using System.Reflection;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuestZoneAPI.Patches
+{
+    public class GameWorldPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(GameWorld).GetMethod("OnGameStarted", BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        public static List<ZoneClass> GetZones()
+        {
+            Logger.LogInfo("Getting Custom Quest Zones...");
+            var zones = Helpers.WebRequestHelper.Get<List<ZoneClass>>("/quests/zones/getZones");
+            Logger.LogInfo(zones.First().ZoneName);
+            return zones;
+        }
+
+        public static void CreatePlaceItemZone(ZoneClass zone)
+        {
+            Logger.LogInfo("Creating Zone...");
+            Logger.LogInfo($"{zone.ZoneName}");
+            GameObject questZone = new GameObject();
+
+            BoxCollider collider = questZone.AddComponent<BoxCollider>();
+            collider.isTrigger = true;
+            Vector3 position = new Vector3(float.Parse(zone.Position.X), float.Parse(zone.Position.Y), float.Parse(zone.Position.Z));
+            Vector3 scale = new Vector3(float.Parse(zone.Scale.X), float.Parse(zone.Scale.Y), float.Parse(zone.Scale.Z));
+            questZone.transform.position = position;
+            questZone.transform.localScale = scale;
+            EFT.Interactive.PlaceItemTrigger scriptComp = questZone.AddComponent<EFT.Interactive.PlaceItemTrigger>();
+            scriptComp.SetId(zone.ZoneId);
+
+            questZone.layer = LayerMask.NameToLayer("Triggers");
+            questZone.name = zone.ZoneId;
+        }
+
+        public static void CreateVisitZone(ZoneClass zone)
+        {
+            Logger.LogInfo("Creating Zone...");
+            Logger.LogInfo(zone.ZoneName);
+            GameObject questZone = new GameObject();
+
+            BoxCollider collider = questZone.AddComponent<BoxCollider>();
+            collider.isTrigger = true;
+            Vector3 position = new Vector3(float.Parse(zone.Position.X), float.Parse(zone.Position.Y), float.Parse(zone.Position.Z));
+            Vector3 scale = new Vector3(float.Parse(zone.Scale.X), float.Parse(zone.Scale.Y), float.Parse(zone.Scale.Z));
+            questZone.transform.position = position;
+            questZone.transform.localScale = scale;
+            EFT.Interactive.ExperienceTrigger scriptComp = questZone.AddComponent<EFT.Interactive.ExperienceTrigger>();
+            scriptComp.SetId(zone.ZoneId);
+
+            questZone.layer = LayerMask.NameToLayer("Triggers");
+            questZone.name = zone.ZoneId;
+        }
+
+        public static void AddZones(List<ZoneClass> zones, string currentLocation)
+        {
+            Logger.LogInfo("Adding all zones...");
+            foreach (ZoneClass zone in zones)
+            {
+                if (zone.ZoneLocation.ToLower() == currentLocation.ToLower())
+                {
+                    switch(Enum.Parse(typeof(ZoneType), zone.ZoneType))
+                    {
+                        case ZoneType.PlaceItem:
+                            Logger.LogInfo(zone.Position.X);
+                            Logger.LogInfo(zone.Rotation.Y);
+                            CreatePlaceItemZone(zone);
+
+                            break;
+                        case ZoneType.Visit:
+                            Logger.LogInfo(zone.Position.X);
+                            Logger.LogInfo(zone.Rotation.Y);
+                            CreateVisitZone(zone);
+                            break;
+                        default:
+                            Logger.LogInfo(zone.Position.X);
+                            Logger.LogInfo(zone.Rotation.Y);
+                            CreateVisitZone(zone);
+                            break;
+                    }
+                } else
+                {
+                    Logger.LogInfo("Zone not in current location");
+                }
+            }
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(GameWorld __instance)
+        {
+            Logger.LogInfo("GameWorld.OnGameStarted [QuestZoneAPI]");
+            try
+            {
+                string loc = __instance.MainPlayer.Location;
+                Logger.LogInfo(loc); // works
+                
+                List<ZoneClass> zones = GetZones();
+                AddZones(zones, loc);
+            } catch (Exception ex)
+            {
+                Logger.LogInfo("GameWorld Error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ src/QuestZoneAPI/Plugin.cs
@@ -0,0 +1,50 @@
+﻿using BepInEx;
+
+namespace QuestZoneAPI
+{
+    [BepInPlugin(Helpers.Constants.pluginGuid, Helpers.Constants.pluginName, Helpers.Constants.pluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("Starting");
+
+            new Patches.GameWorldPatch().Enable();
+
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {Helpers.Constants.pluginName} is loaded!");
+        }
+    }
+
+    public enum ZoneType
+    {
+        PlaceItem,
+        Visit
+    }
+
+    public class ZoneTransform
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+        public string Z { get; set; }
+
+        public ZoneTransform(string x, string y, string z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+
+    public class ZoneClass
+    {
+        public string ZoneId { get; set; }
+        public string ZoneName { get; set; }
+        public string ZoneLocation { get; set; }
+        public string ZoneType { get; set; }
+        public ZoneType ZoneTypeEnum { get; set; }
+        public ZoneTransform Position { get; set; }
+        public ZoneTransform Rotation { get; set; } = new ZoneTransform("0", "0", "0");
+        public ZoneTransform Scale { get; set; } = new ZoneTransform("1", "1", "1");
+    }
+}
